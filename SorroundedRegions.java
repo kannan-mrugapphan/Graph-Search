6313a92b9352266ca22807e22cda40c9f6bc7674
Mrugapphan Kannan <52872043+kannan-mrugapphan@users.noreply.github.com>
2020-06-03 14:28:09 -0500

    Update SorroundedRegions.java

--- SorroundedRegions.java
+++ SorroundedRegions.java
@@ -1,58 +1,65 @@
 // 130.
-
-//time - O(m * n)
-//space - O(m * n) - call stack size when all Os
-
+// time - O(m * n)
+// space - O(m * n)
 //Os in the borders are not fully sorrounded by X
 //sink all Os connected to any O in the border
 //the remaining Os are sorrounded by Xs so flip them and return
 class Solution {
     public void solve(char[][] board) {
         //edge
-        if(board == null || board.length == 0|| board[0].length == 0)
+        if(board == null || board.length == 0 || board[0].length == 0)
         {
             return;
         }
         
-        int m = board.length;
-        int n = board[0].length;
-        
-        //sink Os in the 1st and last row
-        for(int i = 0; i < n; i++)
+        //capture all Os in the borders
+        for(int i = 0; i < board[0].length; i++)
         {
+            //capturing all Os in the 1st and last row
             if(board[0][i] == 'O')
             {
+                //capture this O
                 dfs(board, 0, i);
             }
-            if(board[m - 1][i] == 'O')
+            if(board[board.length - 1][i] == 'O')
             {
-                dfs(board, m - 1, i);
+                //capture this O
+                dfs(board, board.length - 1, i);
             }
         }
-        
-        //sink all Os in 1st and last column
-        for(int i = 0; i < m; i++)
+        for(int i = 0; i < board.length; i++)
         {
+            //capturing all Os in the 1st and last column
             if(board[i][0] == 'O')
             {
+                //capture this O
                 dfs(board, i, 0);
             }
-            if(board[i][n - 1] == 'O')
+            if(board[i][board[0].length - 1] == 'O')
             {
-                dfs(board, i, n - 1);
+                //capture this O
+                dfs(board, i, board[0].length - 1);
             }
         }
         
-        //flip other Os and change -s to Os
-        for(int i = 0; i < m; i++)
+        //iterate through board, if O is found then its is not part of a border O if so it would have been flipped to B -> all neighbors of this O should be X -> if not it would have been captured and flipped to B
+        for(int i = 0; i < board.length; i++)
         {
-            for(int j = 0; j < n; j++)
+            for(int j = 0; j < board[0].length; j++)
             {
                 if(board[i][j] == 'O')
                 {
                     board[i][j] = 'X';
                 }
-                if(board[i][j] == '-')
+            }
+        }
+        
+        //in second pass flip Bs back to Os
+        for(int i = 0; i < board.length; i++)
+        {
+            for(int j = 0; j < board[0].length; j++)
+            {
+                if(board[i][j] == 'B')
                 {
                     board[i][j] = 'O';
                 }
@@ -62,26 +69,27 @@ class Solution {
         return;
     }
     
-    private void dfs(char[][] board, int row, int column) {
-        board[row][column] = '-'; //change to - to mark cell as visited to avoid proessing again, can also have a visited matrix
-        
-        int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        for(int[] direction : dirs) 
+    //time - O(n * m)
+    //space - O(n * m) - max call stack size when all Os in matrix
+    private void dfs(char[][] board, int row, int col) {
+        //base
+        if(board[row][col] != 'O')
         {
-            int nRow = row + direction[0];
-            int nColumn = column + direction[1];
-            
-            if(nRow >= 0 && nRow < board.length && nColumn >= 0 && nColumn < board[0].length)
+            return; //this cell cant be captured -> because the cell doesnt have O
+        }
+        //logic
+        board[row][col] = 'B'; //change border O to B to indicate all regions in all border O
+        int[][] dirs = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}}; // 4 directions
+        for(int[] dir : dirs)
+        {
+            int nRow = row + dir[0]; //neighbor co ordinates
+            int nCol = col + dir[1];
+            if(nRow >= 0 && nRow < board.length && nCol >= 0 && nCol < board[0].length)
             {
-                //neighbour within bounds
-                if(board[nRow][nColumn] == 'O')
-                {
-                    //sink them by dfs
-                    dfs(board, nRow, nColumn);
-                }
+                //neighbor within bounds
+                dfs(board, nRow, nCol);
             }
         }
-        
         return;
     }
 }
