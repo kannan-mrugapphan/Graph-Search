822ac17ac046b96a9f1c36bdb2f9e8453c84e990
Mrugapphan Kannan <52872043+kannan-mrugapphan@users.noreply.github.com>
2020-06-03 13:21:08 -0500

    Update MaxAreaIsland.java

--- MaxAreaIsland.java
+++ MaxAreaIsland.java
@@ -1,23 +1,21 @@
 // 695.
-// time - O(m * n)
-// space - O(m * n) - max call stack size when all cells have 1s
+// time - O(n * m)
+// space - O(n * m) - max call stack size when all 1s in grid
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
         //edge
         if(grid == null || grid.length == 0 || grid[0].length == 0)
         {
-            return 0; //max area of island in an empty grid is 0
+            return 0; //max area in case of an empty grid is 0
         }
-        int maxArea = 0; //initial result
+        int maxArea = 0; //return value
         for(int i = 0; i < grid.length; i++)
         {
             for(int j = 0; j < grid[0].length; j++)
             {
-                if(grid[i][j] == 1) // 1 is found i.e an island is found - find area of this island
-                {
-                    int currentArea = dfs(grid, i, j);
-                    maxArea = Math.max(maxArea, currentArea); //update max if needed
-                }
+                //count the # of 1s in this island, update result if needed
+                int currentArea = dfs(grid, i, j);
+                maxArea = Math.max(maxArea, currentArea);
             }
         }
         return maxArea;
@@ -27,20 +25,20 @@ class Solution {
         //base
         if(grid[row][col] == 0)
         {
-            return 0; //returning as cell is visited
+            return 0; //no island here - so area is 0
         }
         //logic
-        int[][] dirs = {{0, -1}, {0, 1}, {1, 0}, {-1, 0}};
-        grid[row][col] = 0;  //marking as visited to avoid reprocessing
-        int currentArea = 1; //initially area is 1 as the current row, col cell has 1
-        for(int[] direction : dirs)
+        grid[row][col] = 0; //marking as visited as to avoid reprocessing
+        int[][] dirs = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}}; //4 possible neighbors
+        int currentArea = 1; //initially current area is 1 account the initail incoming 1
+        for(int[] dir : dirs)
         {
-            int nRow = row + direction[0]; //nRow, nCol is the next cell that could possibly be visited
-            int nCol = col + direction[1];
+            int nRow = row + dir[0]; //next neighbor co ordinates
+            int nCol = col + dir[1];
             if(nRow >= 0 && nRow < grid.length && nCol >= 0 && nCol < grid[0].length)
             {
                 //neighbor within bounds
-                currentArea += dfs(grid, nRow, nCol); //recurse on neighbor to add to currentArea
+                currentArea += dfs(grid, nRow, nCol); 
             }
         }
         return currentArea;
